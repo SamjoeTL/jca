0f50253ca8a8c9f371acd7167dbbe2c798733a7e
Agus Satrya <agussatrya160800@gmail.com>
2024-11-18 14:32:38 +0800

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,7 +12,7 @@
 
 @section('content')
 <div class="content-wrapper">
-  <div class="content-header row">
+  {{-- <div class="content-header row">
     <div class="content-header-left col-md-12 col-12 mb-2">
       <div class="row breadcrumbs-top">
         <div class="col-12">
@@ -26,9 +26,9 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> --}}
 
-  <section id="component-swiper-multiple">
+  {{-- <section id="component-swiper-multiple">
     <div class="card">
       <div class="card-body">
         <div class="swiper-multiple swiper-container">
@@ -44,7 +44,7 @@
         </div>
       </div>
     </div>
-  </section>
+  </section> --}}
 
   <div class="row">
     <div class="col-md-6">
@@ -63,14 +63,14 @@
         @endforeach
       </div>
     </div>
-    <div class="col-md-6">
+    {{-- <div class="col-md-6">
       <h4 class="mb-1">Home</h4>
       <div class="row match-height">
         @foreach ($homes as $h)
         <div class="col-md-6">
           <div class="card shadow-none">
               <div class="card-body p-1">
-                  {{-- <h4 class="card-title text-white">Success card title</h4> --}}
+                  <h4 class="card-title text-white">Success card title</h4>
                   <img src="{{asset($h->cover)}}" class="rounded img-fluid mb-1" alt="">
                   <p class="card-text font-italic mb-50"> {{$h->nama}}</p>
                   <div class="d-flex justify-content-between">
@@ -83,7 +83,7 @@
         @endforeach
 
       </div>
-    </div>
+    </div> --}}
   </div>
 </div>
 @endsection
