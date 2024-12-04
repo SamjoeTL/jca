ddfcd4fd7c390ffde38d3045e8bf4552ea84c737
SamjoeTL <tegar.terbaik@gmail.com>
2024-12-04 16:54:09 +0800

    ubah tulisan

--- resources/views/admin/Service/create.blade.php
+++ resources/views/admin/Service/create.blade.php
@@ -163,7 +163,7 @@
         <div class="content-header-left col-md-12 col-12 mb-2">
             <div class="row breadcrumbs-top">
                 <div class="col-12">
-                    <h2 class="content-header-title float-left mb-0">Services
+                    <h2 class="content-header-title float-left mb-0">Service
                     </h2>
                     <div class="breadcrumb-wrapper">
                         <ol class="breadcrumb">
